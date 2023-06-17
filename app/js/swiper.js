819bd59086b03a31ff12b9baeda06edf6a5de330
NikolayTolkovanov-front <tnrvlg@mail.ru>
2023-06-17 23:17:22 +0300

    Добавлен слайдер без адаптива

---
+++ app/js/swiper.js
@@ -0,0 +1,18 @@
+const paginationItems = ["Dogie", "Падение", "Возвращение"]
+
+const customPaginationItem = (index, className) => {
+  return '<span class="' + className + '">' + paginationItems[index] + "</span>";
+}
+
+const mainSwiper = new Swiper('.main__swiper-container', {
+  direction: "vertical",
+  
+  // loop: true,
+  pagination: {
+    el: ".main__swiper-pagination",
+    clickable: true,
+    type: "bullets",
+    renderBullet: customPaginationItem,
+  },
+
+});
